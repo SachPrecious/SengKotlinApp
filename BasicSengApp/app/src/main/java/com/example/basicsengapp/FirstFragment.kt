ac6f1a76af820f0a681d48b3ad30528e80a42b45
SachPrecious <sachithrabandara49@gmail.com>
2022-03-21 00:00:27 +0530

    Assignment

--- BasicSengApp/app/src/main/java/com/example/basicsengapp/FirstFragment.kt
+++ BasicSengApp/app/src/main/java/com/example/basicsengapp/FirstFragment.kt
@@ -2,6 +2,7 @@ package com.example.basicsengapp
 
 import android.os.Bundle
 import android.telecom.Call
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -37,28 +38,44 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.buttonFirst.setOnClickListener{
 
-        val user = userAPIService.getUser("1");
+            val editText = binding.editText.editableText
+            val user = userAPIService.getUser(editText.toString());
+
+           // val user = userAPIService.getUser("1");
+            Log.i("FirstFragment","buttonFirst")
+
+            user.enqueue(object : retrofit2.Callback<User> {
+                override fun onResponse(call: retrofit2.Call<User>, response: Response<User>) {
+                    val body =response.body()
+                    body?.let {
+                        Log.i("FirstFragment Name :",it.name)
+                        binding.textviewUserID.text="User ID "+it.id
+                        binding.textviewUserName.text="Name:  "+it.username
+                        binding.textviewUserEmail.text="Email:  "+it.email
+                        //binding.textviewUserWebsite.text="Website:  "+it.website
+
+                    }
+
+                }
+
+                override fun onFailure(call: retrofit2.Call<User>, t: Throwable) {
+                    Log.i("FirstFragment Name :",t.message!!)
+                }
 
 
-        user.enqueue(object : retrofit2.Callback<User> {
-            override fun onResponse(call: retrofit2.Call<User>, response: Response<User>) {
-                TODO("Not yet implemented")
-            }
+            })
 
-            override fun onFailure(call: retrofit2.Call<User>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-
-        })
-
-
-
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+
+
+
+
+     /*   binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }*/
     }
 
     override fun onDestroyView() {
